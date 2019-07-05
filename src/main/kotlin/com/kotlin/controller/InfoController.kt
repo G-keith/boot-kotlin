a3bc55fadc58d4faab2137335f1f2846c86301eb
keith <G-keith>
2019-07-05 10:31:00 +0800

    kotlin-gradle-swagger

--- src/main/kotlin/com/kotlin/controller/InfoController.kt
+++ src/main/kotlin/com/kotlin/controller/InfoController.kt
@@ -3,6 +3,9 @@ package com.kotlin.controller
 import com.baomidou.mybatisplus.core.metadata.IPage
 import com.kotlin.entity.Info
 import com.kotlin.service.IInfoService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiImplicitParam
+import io.swagger.annotations.ApiOperation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.*
  *@date 2019/7/4
  *@version 1.0
  */
-
+@Api(tags = ["个人信息"])
 @RestController
 @RequestMapping("/info")
 class InfoController{
@@ -25,6 +28,8 @@ class InfoController{
     }
 
     @GetMapping("/info")
+    @ApiOperation(value = "根据主键id查询个人信息")
+    @ApiImplicitParam(name = "id",value = "主键id",dataType = "int",required = true)
     fun selectInfo(id:Int):Info{
         return iInfoService.selectById(id)
     }
